3d82fba6e26211ae857be3de3fbe131bdf532c07
Depech Vogel <eithreekf@gmail.com>
2018-11-01 16:46:46 +0000

    Update wait_for_disks.ps1

--- wait_for_disks.ps1
+++ wait_for_disks.ps1
@@ -1,4 +1,10 @@
-    while ((Get-Disk).Count -lt ${Count}) {
-        Write-Host "$(Get-Date -uformat "%Y-%m-%d-%H%M%S") : Waiting on ${Count} EBS volumes to be attached. Sleeping 60 seconds.";
-        Start-Sleep 60;
-        }
+    Do{
+        $S = 60;
+        Write-Host "$(Get-Date -uformat "%Y-%m-%d-%H%M%S") : Waiting on ${Count} EBS volumes to be attached. Current disk count is $((Get-Disk).Count).";
+        Write-Host "$(Get-Date -uformat "%Y-%m-%d-%H%M%S") : Current disks.";
+        Get-Disk | Sort-Object | ForEach-Object { 
+            Write-Host $(Get-Date -uformat "%Y-%m-%d-%H%M%S") : $_.Number $_.SerialNumber $_.HealthStatus $_.OperationalStatus;
+            }
+        Write-Host "$(Get-Date -uformat "%Y-%m-%d-%H%M%S") : Sleeping for $S seconds.";
+        Start-Sleep $S;
+        } Until ((Get-Disk).Count -eq $Count)
