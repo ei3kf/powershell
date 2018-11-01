17c5645b000e3d6c8c2213e0b5235fd2651242d6
Depech Vogel <eithreekf@gmail.com>
2018-11-01 16:47:48 +0000

    Update unintialise_disks.ps1

--- unintialise_disks.ps1
+++ unintialise_disks.ps1
@@ -1,6 +1,10 @@
-    (Get-Volume).DriveLetter | Sort-Object | ForEach-Object {
-        If ( $_ -ne "C" ){
-            Write-Host "$(Get-Date -uformat "%Y-%m-%d-%H%M%S") : Un-initializing $_.";
-            Remove-Partition -DriveLetter $_ -Confirm:0 -Verbose;
+    ## Uninitialise disks except for C:\
+    Get-Volume | Where-Object {$_.DriveLetter -ne "C"} | ForEach-Object{ 
+        Write-Host "$(Get-Date -uformat "%Y-%m-%d-%H%M%S") : Un-initializing drive " $_.DriveLetter;
+        try{
+            Remove-Partition -DriveLetter $_.DriveLetter -Confirm:0 -Verbose; Start-Sleep 30;
+            }
+        catch{
+            Write-Host "$(Get-Date -uformat "%Y-%m-%d-%H%M%S") : No partitions to remove";
+            }
         }
-    }
